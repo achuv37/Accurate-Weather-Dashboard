7a94557e8508eb46c2f05ee16978fc6e2eaac7f1
Achu Ajesh <achuv37@gmail.com>
2022-03-18 10:33:30 -0400

    Add a function allow user to search for a city

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,3 +6,18 @@
 // 5. Display five day forecast includes Date, Temperature, Humidity.
 // 6. Set the local storage to save the city search history.
 // 7. Error Handling.
+
+// Declaring Variables.
+var cityName = "";
+// function to allow user to search for a city.
+function searchCity () {
+  cityName = $("input").val().trim();
+// Creating buttons dynamically as the user enters more cities to search.
+  var cityList = $("button");
+  cityList.addClass("list-group-item list-group-item-action");
+  cityList.text(cityName);
+// The prepend() method inserts specified content at the beginning of the selected elements.
+  $("ul").prepend(cityList);
+// After the user's city is saved,clearing the input fields.
+  $("input").val("");
+}
