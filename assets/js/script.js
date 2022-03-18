28c04cc57ef670171b201e9c7083fd520e66139e
Achu Ajesh <achuv37@gmail.com>
2022-03-17 23:57:11 -0400

    Add pseudo code to the script file

---
+++ assets/js/script.js
@@ -0,0 +1,8 @@
+// Pseudo code
+// 1. Declare the variables.
+// 2. Create a function to allow user to search for a city and the weather is displayed using Open Weather Map API.
+// 3. Create another function for displaying current weather data includes Date,Temperature,Humidity,Wind speed and UV-Index.
+// 4. UV-Index change its color based on the conditions.
+// 5. Display five day forecast includes Date, Temperature, Humidity.
+// 6. Set the local storage to save the city search history.
+// 7. Error Handling.
