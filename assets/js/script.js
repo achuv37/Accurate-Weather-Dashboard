fcd908eefacbcac4f936f984d71d237a73ea4197
Achu Ajesh <achuv37@gmail.com>
2022-03-19 12:56:11 -0400

    Updated script file

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,37 +1,53 @@
-// Pseudo code
-// 1. Declare the variables.
-// 2. Create a function to allow user to search for a city and the weather is displayed using Open Weather Map API.
-// 3. Create another function for displaying current weather data includes Date,Temperature,Humidity,Wind speed and UV-Index.
-// 4. UV-Index change its color based on the conditions.
-// 5. Display five day forecast includes Date, Temperature, Humidity.
-// 6. Set the local storage to save the city search history.
-// 7. Error Handling.
+var userFormEl = document.querySelector("#city-form");
+var nameInputEl = document.querySelector("#city-name");
 
-// Declaring Variables.
-var cityName = "";
-// function to allow user to search for a city.
-function searchCity () {
-  cityName = $("input").val().trim();
-// Creating buttons dynamically as the user enters more cities to search.
+var getUserInput = function(cityName) {
+  // format the github api url
+  var apiUrl = "https://api.openweathermap.org/data/2.5/weather?q="+ cityName +"&appid=3e40f415c86db2e6bfc1aa88c8631d39";
+
+  // make a request to the url
+  fetch(apiUrl).then(function(response) {
+    response.json().then(function(data) {
+      var lan = data.coord.lat;
+      var lon = data.coord.lon;
+      console.log(lan,lon);
+      $("#city").text(data.name);
+      $("#date").text(new Date(data.dt*1000));
+      localStorage.setItem("city", data.name);
+      getWeatherData(lan,lon);
+    });
+  });
+}
+function getWeatherData(lan,lon) {
+  var apiUrl1 = "https://api.openweathermap.org/data/2.5/onecall?lat=" + lan + "&lon="  
+      + lon + "&appid=3e40f415c86db2e6bfc1aa88c8631d39";
+  fetch(apiUrl1).then(function(response) {
+    response.json().then(function(data) {
+    console.log(data);
+    $("#current-data").css({"display": "block"});
+    
+    
+        
+    });
+  });
+
+}
+
+
+var formSubmitHandler = function(event) {
+  event.preventDefault();
+// get value from input element
+var cityName = nameInputEl.value.trim();
+
+if (cityName) {
+  getUserInput(cityName);
   var cityList = $("button");
   cityList.addClass("list-group-item list-group-item-action");
   cityList.text(cityName);
-// The prepend() method inserts specified content at the beginning of the selected elements.
   $("ul").prepend(cityList);
-// After the user's city is saved,clearing the input fields.
-  $("input").val("");
+  nameInputEl.value = "";
+} else {
+  alert("Please enter a city name");
 }
-
-// Add eventListener to the button element.
-$("#city-form").submit(function(event) {
-  event.preventDefault();
-  searchCity();
-});
-$("#button-submit").click(function(event) {
-  event.preventDefault();
-  searchCity(); 
-});
-$("ul").on("click","button",function() {
-  cityName = $(this).text();
-  console.log(cityName);
-})
+};
+userFormEl.addEventListener("submit", formSubmitHandler); 
